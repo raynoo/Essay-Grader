c06554952c477c1f9da0fed14b52e1b1fc7a4d94
Renu Srinivasan <raynoo@gmail.com>
2013-04-05 03:13:19 -0500

    more files for setup

--- src/nlp/grader/main/Main.java
+++ src/nlp/grader/main/Main.java
@@ -1,14 +1,15 @@
 package nlp.grader.main;
 
+import java.io.IOException;
 import java.io.StringReader;
 import java.util.List;
 
-import edu.stanford.nlp.ling.CoreLabel;
-import edu.stanford.nlp.objectbank.TokenizerFactory;
-import edu.stanford.nlp.parser.lexparser.LexicalizedParser;
-import edu.stanford.nlp.process.CoreLabelTokenFactory;
-import edu.stanford.nlp.process.PTBTokenizer;
-import edu.stanford.nlp.trees.Tree;
+import org.apache.lucene.search.spell.LuceneDictionary;
+import org.apache.lucene.search.spell.SpellChecker;
+
+import edu.stanford.nlp.ling.HasWord;
+import edu.stanford.nlp.ling.TaggedWord;
+import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 
 public class Main {
 
@@ -17,19 +18,35 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		LexicalizedParser lp = LexicalizedParser.loadModel("edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz");
-		lp.setOptionFlags(new String[]{"-outputFormat", "penn,typedDependenciesCollapsed", "-retainTmpSubcategories"});
-		TokenizerFactory<CoreLabel> tokenizerFactory = PTBTokenizer.factory(new CoreLabelTokenFactory(), "");
 		
-		new Main().processSentence("Following exception caught during parsing.", tokenizerFactory, lp);
+		Main main = new Main();
+//		main.spellCheck();
+		List<TaggedWord> twords = main.getWordTags("The strongest rain ever recorded " +
+				"in India shut down the financial hub of Mumbai, snapped communication lines, " +
+				"closed airports and forced thousands of people to sleep in their offices or walk " +
+				"home during the night, officials said today.");
+		
+		for(TaggedWord s : twords)
+			System.out.println("Word: " + s.value() + " Tag: " + s.tag());
 	}
 	
-	public Tree processSentence(String sentence, TokenizerFactory<CoreLabel> tokenizerFactory,
-			LexicalizedParser lp) {
-		List<CoreLabel> rawWords = tokenizerFactory.getTokenizer(new StringReader(sentence)).tokenize();
-		Tree bestParse = lp.parseTree(rawWords);
-		System.out.println(bestParse.toString());
-		return bestParse;
+	public List<TaggedWord> getWordTags(String sentence) {
+		MaxentTagger tagger = null;
+		try {
+			tagger = new MaxentTagger("taggers/english-left3words-distsim.tagger");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		List<List<HasWord>> tokenList = MaxentTagger.tokenizeText(new StringReader(sentence));
+		return tagger.tagSentence(tokenList.get(0));
+	}
+	
+	public void spellCheck() {
+		
 	}
 
 }

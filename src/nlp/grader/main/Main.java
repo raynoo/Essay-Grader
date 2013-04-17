2db3a1e0ef39176d6989ea775be5e1080250f6f8
Renu Srinivasan <raynoo@gmail.com>
2013-04-16 20:40:53 -0500

    Main is now the Grader. stick to this.

--- src/nlp/grader/main/Main.java
+++ src/nlp/grader/main/Main.java
@@ -17,48 +17,67 @@ import nlp.grader.utils.Reader;
 public class Main {
 
 	/**
-	 * Read from input file and passes essay to Grader to get final grade.
-	 * 
+	 * Processes the essay and calculates grade.
 	 * @param args
+	 * (@return final grade)
 	 */
 	public static void main(String[] args) {
 
+		File corpusPath = new File("test_corpus");
+		Main grader = new Main();
 
-
-				File corpusPath = new File("test_corpus");
-				
-				for(File file : corpusPath.listFiles()) {
-					if(file.isFile()) {
-						System.out.println(file.getName());
-						Essay essay = new Essay(Reader.readTestingFile(file.getAbsolutePath()));
-						System.out.println(essay.getSentences());
-						
-//						for(Sentence sentence :essay.getSentences()){
-//							Criteria.isVerbAgreeing(sentence);
-//							Criteria.isVerbNounAgreeing(sentence);
-//							WordOrder.getWordOrderErrors(sentence);
-//							
-//							System.out.println(sentence);
-//							System.out.println(sentence.getTaggedWords());
-//							sentence.printAllErrors();
-//							
-//							
-//		
-//						}
-//						System.out.println("\n--------------------------------------------\n");
-						
-					}
-				}
-
+		for(File file : corpusPath.listFiles()) {
+			if(file.isFile()) {
+				System.out.println(file.getName());
+				Essay essay = new Essay(Reader.readTestingFile(file.getAbsolutePath()));
+//				System.out.println(essay.getSentences());
+				grader.checkEssay(essay);
+			}
+		}
 
 //		List<String> temp = new ArrayList<String>(); 
 //		temp.add("My name is Aurora Garcia I likes English to learn because I understands with my supervisor");
 //		Essay es = new Essay(temp);	
-
-		//		Sentence s = new Sentence("I like to running");
-		//		WordOrder.getWordOrderErrors(s);
-		////		System.out.println("\n" + Criteria.isVerbNounAgreeing(s));
-		////		System.out.println(Criteria.isVerbAgreeing(s));	
+//
+//		Sentence s = new Sentence("I like to running");
+//		WordOrder.getWordOrderErrors(s);
+//		System.out.println("\n" + Criteria.isVerbNounAgreeing(s));
+//		System.out.println(Criteria.isVerbAgreeing(s));	
 	}
-
+	
+	
+	/**
+	 * Perform criteria checks for each sentence in essay.
+	 * @param essay
+	 * @return Points object
+	 */
+	public void checkEssay(Essay essay) {
+		for(Sentence s : essay.getSentences()) {
+			
+			System.out.println("\n" + s);
+			
+			//all criteria checks
+			WordOrder.getWordOrderErrors(s);
+			Criteria.isVerbNounAgreeing(s);
+			Criteria.isVerbAgreeing(s);
+			
+			s.printAllErrors();
+			
+			System.out.println("\n--------------------------------------------\n");
+		}
+	}
+	
+	/**
+	 * Assign a point (1-5) for each criteria according to number of errors. 
+	 */
+	private void calculatePoints() {
+		
+	}
+	
+	/**
+	 * Calculate the final grade as per essay Points.
+	 */
+	private void calculateFinalGrade() {
+		
+	}
 }

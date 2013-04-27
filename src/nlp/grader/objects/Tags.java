b1c36f8630d07bae2333c889bd3e273ef3d874f4
Kevin Varkey <kevingeorge26@gmail.com>
2013-04-27 16:41:28 -0500

    tag apis

--- src/nlp/grader/objects/Tags.java
+++ src/nlp/grader/objects/Tags.java
@@ -1,5 +1,6 @@
 package nlp.grader.objects;
 
+import java.util.Arrays;
 import java.util.HashSet;
 
 import nlp.grader.utils.Reader;
@@ -11,12 +12,24 @@ import nlp.grader.utils.Reader;
  */
 public class Tags {
 
+	static{
+		String[] fePrp = {"she","her","hers"};
+		String[] maPrp = {"he","his","him"};
+		String[] plPrp = {"we","they","them","their","those","us"};
+	}
+	
 	private static HashSet<String> verbTags = null;
 	private static HashSet<String> nounTags = null;
-	private static HashSet<String> femalePRPWords = null;
+	
+	private static HashSet<String> femalePRPWords = null; // pronoun
 	private static HashSet<String> malePRPWords = null;
 	private static HashSet<String> pluralPRPWords = null;
 	
+	private static HashSet<String> femaleWords = null;
+	private static HashSet<String> maleWords = null;
+	
+	
+		
 	public static HashSet<String> getVerbTags() {
 		if(verbTags == null) {
 			verbTags = new HashSet<String>();
@@ -32,5 +45,33 @@ public class Tags {
 		}
 		return nounTags;
 	}
+	
+	public static boolean isFemalePrp(String word)
+	{
+		return femalePRPWords.contains(word);
+	}
+	
+	public static boolean isMalePrp(String word)
+	{
+		return malePRPWords.contains(word);
+	}
+	
+	public static boolean isPluralPrp(String word)
+	{
+		return pluralPRPWords.contains(word);
+	}
+	
+	public static boolean isFemaleWord(String word)
+	{
+		return femaleWords.contains(word);
+	}
+	
+	public static boolean isMaleWord(String word)
+	{
+		return maleWords.contains(word);
+	}
+	
+	
+	
 	
 }

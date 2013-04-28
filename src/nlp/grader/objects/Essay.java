3ba2273a7a590a9435fac039cebf92f71f2aafd6
Kevin Varkey <kevingeorge26@gmail.com>
2013-04-28 18:07:14 -0500

    original sentence

--- src/nlp/grader/objects/Essay.java
+++ src/nlp/grader/objects/Essay.java
@@ -12,79 +12,83 @@ import edu.stanford.nlp.trees.Tree;
 
 public class Essay {
 
+	private List<Sentence> originalSentence;
 	private List<Sentence> essay;
 	private Points essayPoints;
 	int twoBScore;
-	
-	
+
+
 	public Essay(List<String> sentences) {
 
 		this.essay = new ArrayList<Sentence>();
-		
+		this.originalSentence = new ArrayList<Sentence>();
+
+
 		for(String s : sentences)
 		{
 			try{
-			String lastSentence = "";
-			
-			s = s.replaceAll("\\s+,", ",");
-			Sentence sentence = new Sentence(s);
+				originalSentence.add(new Sentence(s));
+				String lastSentence = "";
 
-			Queue<Tree> que = new LinkedList<Tree>();
-			que.add(sentence.getParseTree());
-			Iterator<Tree> subChildren = sentence.getParseTree().iterator();
+				s = s.replaceAll("\\s+,", ",");
+				Sentence sentence = new Sentence(s);
 
-			while( subChildren!= null && subChildren.hasNext() )
-			{
-				Tree t = subChildren.next();
-				if(t.value().equals("SBAR"))
+				Queue<Tree> que = new LinkedList<Tree>();
+				que.add(sentence.getParseTree());
+				Iterator<Tree> subChildren = sentence.getParseTree().iterator();
+
+				while( subChildren!= null && subChildren.hasNext() )
 				{
-					que.add(t);
-				}
-			}
-
-			while(!que.isEmpty())
-			{
-				String newSentence = "";
-				Iterator<Tree> t = que.remove().iterator();
-				boolean foundS = false;
-
-				while(t.hasNext())
-				{
-					Tree temp = t.next();
-					if(temp == que.peek())
+					Tree t = subChildren.next();
+					if(t.value().equals("SBAR"))
 					{
-						break;
+						que.add(t);
 					}
-					else
-					{
-						if(temp.value().equals("S") || temp.value().equals("ROOT"))
-						{
-							foundS = true;
-						}
-						else if(temp.isLeaf() && foundS)
-						{
-							if(newSentence.equals(""))
-								newSentence +=temp.value();
-							else if(temp.value().equals(","))
-								newSentence = newSentence + temp.value();
-							else
-								newSentence = newSentence + " " + temp.value();
-						}
-					}
-				}				
-
-				lastSentence = newSentence;
-				
-				if(newSentence.length() > 1)
-				{
-					
-					this.essay.add(new Sentence(newSentence));
 				}
-			}
 
-			if( s.indexOf(lastSentence) + lastSentence.length() +1 < s.length())
-				this.essay.add(new Sentence( s.substring( s.indexOf(lastSentence) + lastSentence.length() +1 ) ));
-		}
+				while(!que.isEmpty())
+				{
+					String newSentence = "";
+					Iterator<Tree> t = que.remove().iterator();
+					boolean foundS = false;
+
+					while(t.hasNext())
+					{
+						Tree temp = t.next();
+						if(temp == que.peek())
+						{
+							break;
+						}
+						else
+						{
+							if(temp.value().equals("S") || temp.value().equals("ROOT"))
+							{
+								foundS = true;
+							}
+							else if(temp.isLeaf() && foundS)
+							{
+								if(newSentence.equals(""))
+									newSentence +=temp.value();
+								else if(temp.value().equals(","))
+									newSentence = newSentence + temp.value();
+								else
+									newSentence = newSentence + " " + temp.value();
+							}
+						}
+					}				
+
+					lastSentence = newSentence;
+
+					if(newSentence.length() > 1)
+					{
+
+						this.essay.add(new Sentence(newSentence));
+					}
+				}
+
+				if( s.indexOf(lastSentence) + lastSentence.length() +1 < s.length())
+					this.essay.add(new Sentence( s.substring( s.indexOf(lastSentence) + lastSentence.length() +1 ) ));
+			}
 			catch(Exception e)
 			{
 				//System.out.println("not able to fragment the sentence " + s);
@@ -98,6 +102,11 @@ public class Essay {
 	public List<Sentence> getSentences() {
 		return this.essay;
 	}
+	
+	public List<Sentence> getOriginalSentence()
+	{
+		return originalSentence;
+	}
 
 	public Points getPoints() {
 		return this.essayPoints;
@@ -110,7 +119,7 @@ public class Essay {
 	public void setTwoBScore(int twoBScore) {
 		this.twoBScore = twoBScore;
 	}
-	
-	
+
+
 
 }
